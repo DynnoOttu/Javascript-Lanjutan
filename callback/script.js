c2ebfe777823f436d18d3687ac66c7c3ec8d6f1a
DynnoOttu <dynnoottu968@gmail.com>
2023-02-06 16:36:53 +0800

    first commit

---
+++ callback/script.js
@@ -0,0 +1,13 @@
+// callback
+// synchronous function
+// function halo(nama){
+//     alert(`Hello ${nama}`)
+// }
+
+// function tampilkanPesan(callback){
+//     const nama = prompt('Masukan nama')
+//     callback(nama)
+// }
+
+// tampilkanPesan(halo)
+
